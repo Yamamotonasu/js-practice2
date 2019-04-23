224ebb53a4425afb97361004826aeba552c58355
Yamamotonasu <deep.blue.deep@outlook.jp>
2019-04-23 22:02:22 +0900

    redux テスト

--- app/javascript/components/counter.jsx
+++ app/javascript/components/counter.jsx
@@ -2,40 +2,19 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export default class Counter extends Component {
-  constructor (props){
-    super(props)
-    this.state = {
-      count: props.count
-    }
-  }
-
-  handleUp = () => {
-    this.setState({
-      count: ++this.state.count
-    })
-  }
-
-  handleDown = () => {
-    this.setState({
-      count: --this.state.count
-    })
-  }
-
   render () {
     return(
       <div>
-        <div>{ this.state.count }</div>
-        <button onClick={this.handleUp} >+</button>
-        <button onClick={this.handleDown} >-</button>
+        <div>{ this.props.count }</div>
+        <button onClick={this.props.onClickCountUp} >+</button>
+        <button onClick={this.props.onClickCountDown} >-</button>
       </div>
     )
   }
 }
 
-Counter.defaultProps = {
-  count: 0
-}
-
 Counter.propTypes = {
-  count: PropTypes.number
+  count: PropTypes.number.isRequired,
+  onClickCountUp: PropTypes.func.isRequired,
+  onClickCountDown: PropTypes.func.isRequired
 }
